f755231fe519f8776bf6f2e4371a67d06281b5be
Agyey Arya <agyey27@yahoo.co.in>
2020-12-06 16:00:02 +0530

    Error Messages

--- translator.py
+++ translator.py
@@ -12,12 +12,18 @@ def translate(input_language, output_language, word, f):
     # Get Translation
     url = f'https://context.reverso.net/translation/{direction}/{word}'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers)
+    except:
+        return 'network_issue'
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        translations = [a.text.strip() for a in soup.find('div', {'id': 'translations-content'}).find_all('a')]
-        examples = [ele.text.strip() for ele in
-                    soup.find('section', {'id': 'examples-content'}).find_all('span', {'class': 'text'})]
+        try:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            translations = [a.text.strip() for a in soup.find('div', {'id': 'translations-content'}).find_all('a')]
+            examples = [ele.text.strip() for ele in
+                        soup.find('section', {'id': 'examples-content'}).find_all('span', {'class': 'text'})]
+        except:
+            return 'translation_issue'
         # Write to File
         f.write(f"{output_language.title()} Translations:\n")
         f.write("\n".join(translations[:5]))
@@ -34,6 +40,9 @@ def translate(input_language, output_language, word, f):
             print(examples[i])
             print(examples[i + 1])
             print()
+    else:
+        return 'translation_issue'
+    return 'Success'
 
 # Get Translation Direction and word
 args = sys.argv
@@ -43,7 +52,19 @@ with open(f'{word}.txt', 'w', encoding="utf-8") as f:
         for output_language in languages:
             if output_language.lower() == input_language.lower():
                 continue
-            translate(input_language, output_language, word, f)
+            response = translate(input_language, output_language, word, f)
+            if response == 'network_issue':
+                print('Something wrong with your internet connection')
+                break
+            elif response == 'translation_issue':
+                print(f'Sorry, unable to find {word}')
+                break
     else:
         if output_language.title() in languages:
-            translate(input_language, output_language, word, f)
+            response = translate(input_language, output_language, word, f)
+            if response == 'network_issue':
+                print('Something wrong with your internet connection')
+            elif response == 'translation_issue':
+                print(f'Sorry, unable to find {word}')
+        else:
+            print(f"Sorry, the program doesn't support {output_language}")
